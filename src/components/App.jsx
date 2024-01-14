78410c25782aaf464721e638aca60e4ee31e21bd
Liubov <drogaltseva92@gmail.com>
2024-01-14 17:09:34 +0100

    task 2 phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from 'nanoid'
 import Form from './Form/Form';
 import Title from "./Form/Title";
@@ -9,49 +9,52 @@ import Filter from "./Form/FilterSearch";
 
 
 
-export class App extends Component {
-  state = {
-    contacts: [],
+export const App = () => {
+ 
+  const [values, setValues] = useState({
+    contacts: JSON.parse(localStorage.getItem('contacts')) || [],
     // name: '',
     // number: '',
     filter: '',
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
-
-  componentDidMount() {
-    const dataContacts = JSON.parse(localStorage.getItem('contacts'))
-    if (dataContacts && dataContacts.length) {
-      this.setState({
-        contacts: dataContacts,
-      })
-    }
-  } 
+  })
+  const { contacts, filter } = values;
 
 
-  
-  // checkContacts = (name) => {
-  //   return this.state.contacts.findIndex(el => el.name === name);
+  useEffect(() => {
+     localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.contacts !== this.state.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  //   }
   // }
 
-  
-  
+  // componentDidMount() {
+  //   const dataContacts = JSON.parse(localStorage.getItem('contacts'))
+  //   if (dataContacts && dataContacts.length) {
+  //     this.setState({
+  //       contacts: dataContacts,
+  //     })
+  //   }
+  // } 
 
-  onChangeInput = (event) => {
+
+  
+ const onChangeInput = (event) => {
     const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+   setValues({
+     ...filter,
+     ...values,
+     [name]: value
+   });
   }
 
-  onClickSubmit = (event) => {
+  const onClickSubmit = (event) => {
     //console.log(event.target)
     event.preventDefault(); 
 
-    const { name } = this.state;
-    const isExist = this.state.contacts.findIndex(el => el.name.toLocaleLowerCase().trim() === name.toLocaleLowerCase().trim());
+    const { name } = values;
+    const isExist = values.contacts.findIndex(el => el.name.toLocaleLowerCase().trim() === name.toLocaleLowerCase().trim());
     //console.log(isExist)
 
     if (isExist >= 0) {
@@ -60,22 +63,23 @@ export class App extends Component {
     }
 
 
-    this.setState({
+    setValues({
       contacts: [
-        ...this.state.contacts,
+        ...contacts,
         {
-          number: this.state.number,
-          name: this.state.name,
+          number: values.number,
+          name: values.name,
           id: nanoid(),
         }
       ],
       name: '',
-      number: ''
+      number: '',
+      filter: '',
     });
     event.currentTarget.reset()
   }
   
-  onClickDelete = (event) => {
+  const onClickDelete = (event) => {
     const selectContact = event.currentTarget.id
     //console.log(selectContact)
     /*----------use method slice----------------*/
@@ -83,29 +87,24 @@ export class App extends Component {
     // console.log(updateContactAfterDelete)
     // this.setState({})
     /*---------------------use method filter-----------*/
-    const updateContactAfterDelete = this.state.contacts.filter(item => item.id !== selectContact)
+    const updateContactAfterDelete = contacts.filter(item => item.id !== selectContact)
     //console.log(updateContactAfterDelete)
-    this.setState({
+    setValues((prevValues) => ({
+      ...prevValues,
       contacts: updateContactAfterDelete,
-    })
+    }))
   }
 
 
-  render() {
-    const contactsArr = Object.values(this.state.contacts)
-    //console.log(contactsArr)
-    const filterContacts = contactsArr.filter((item => item.name.toLocaleLowerCase().includes(this.state.filter.toLocaleLowerCase())))
+   const contactsArr = Object.values(contacts);
+    const filterContacts = contactsArr.filter((item => item.name?.toLocaleLowerCase().includes(filter?.toLocaleLowerCase())))
     //console.log(filterContacts)
     return <div className="App">
      <Title title="Phonebook">
-        <Form onChangeInput={this.onChangeInput} onClickSubmit={this.onClickSubmit} />
+        <Form onChangeInput={onChangeInput}  onClickSubmit={onClickSubmit}/>
       </Title> 
-       {this.state.contacts.length !== 0 ? <Filter onChangeInput={this.onChangeInput} /> : ''}
-      {this.state.contacts.length !== 0 ? <ContactList filterContacts={filterContacts} onChangeInput={this.onChangeInput} onClickDelete={this.onClickDelete} /> : ''}
-     
-      
-     
+       {contacts.length !== 0? <Filter onChangeInput={onChangeInput} /> : ''}
+      {contacts.length !== 0 ? <ContactList filterContacts={filterContacts} onChangeInput={onChangeInput} onClickDelete={onClickDelete} /> : ''}
     </div>
   }
   
-}
